9b0fa02ccf6d91391ffc7f49544842b82c861f1d
Jimena-val <andrea.valencia@iest.edu.mx>
2026-03-25 18:37:35 -0600

    conectar preferencias con ProfileView para q recupere los datos guradados

--- app/src/main/java/com/example/moviles/persistencia/ProfileView.kt
+++ app/src/main/java/com/example/moviles/persistencia/ProfileView.kt
@@ -3,17 +3,22 @@ package com.example.moviles.persistencia
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import kotlinx.coroutines.launch
 
 @Composable
 @Preview(showBackground = true)
@@ -21,6 +26,15 @@ fun ProfileView(){
     var nombre by remember { mutableStateOf("") }
     var edad by remember { mutableStateOf(0) }
     var hasPet by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+    val preferences = Preferencias(context)
+    val corrutina = rememberCoroutineScope()
+    //Recuperar los datos
+    var nombreGuardado = preferences.name.collectAsState("")
+    var mascotaGuardada = preferences.hasPet.collectAsState(false)
+    var edadGuardada = preferences.age.collectAsState(0)
+
+
     Column(modifier = Modifier
         .fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -29,14 +43,24 @@ fun ProfileView(){
         Text(text="Mi Perfil")
         TextField(
             value = nombre,
-            onValueChange = {},
+            onValueChange = {nombre = it},
             label = { Text(text="Nombre") }
         )
         TextField(
             value = edad.toString(),
-            onValueChange = {},
-            label = { Text(text="Nombre") }
+            onValueChange = {
+                if(it.isNotBlank() && it.toIntOrNull() != null){
+                    edad = it.toInt()
+                }
+            }
         )
         Switch(hasPet, onCheckedChange = {hasPet = it})
+        Button(onClick = {
+            corrutina.launch {
+                preferences.guardarDatosPersonal(edad, nombre, hasPet)
+            }
+        }) {
+            Text(text="Guardar")
+        }
     }
 }
